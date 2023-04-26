5d36a7b42f36adb8c45df7a578ce75b49eb3f4ea
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-04-26 16:54:48 +0100

    components folder added

---
+++ src/Components/Main.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+
+const Main = () => {
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=ca13fd5cad80951f9021aee9c8656c29';
+
+  return (
+    <div>
+      Main
+    </div>
+  )
+}
+
+export default Main
