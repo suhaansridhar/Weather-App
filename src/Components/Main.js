454887d7614125461031c59a09c75b9f8ce72b6a
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-04-26 18:36:31 +0100

    adding styles

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import {AiOutlineSearch} from 'react-icons/ai';
 
 
 const Main = () => {
@@ -9,18 +10,30 @@ const Main = () => {
 
     
 
-    function sub(){
+    function search(){
         axios.get(url).then((response) => {
             setData(response.data)
             console.log(response.data)
         })
     }
 
+    // <input type="text" onChange={event => setLocation(event.target.value)} value={location}/>
+    //       <button onClick={sub}>submit</button>
+    //       {data.main? <h2>{data.main.temp}</h2> : null}
+
     return (
-      <div>
-          <input type="text" onChange={event => setLocation(event.target.value)} value={location}/>
-          <button onClick={sub}>submit</button>
-          {data.main? <h2>{data.main.temp}</h2> : null}
+      <div className='main--container'>
+        <div className="main--container--inner">
+            <div className="search--container">
+                <input type="text" value={location} onChange={event => setLocation(event.target.value)}/>
+                <button onClick={search}><AiOutlineSearch /></button>
+            </div>
+            <div className="inner--container--1">
+                <h3>{data.name}</h3>
+                <div className="weather--temperature">{data.main? data.main.temp : null}*</div>
+                <div className="high--low">H:{data.main? data.main.temp_max : null} L:{data.main? data.main.temp_min : null}</div>
+            </div>
+        </div>
       </div>
     )
 }
