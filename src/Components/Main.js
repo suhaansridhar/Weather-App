0ca91b2e1f8ef1850513596655ce17233488cc5a
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-04-26 17:27:54 +0100

    added api fetching code

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,13 +3,26 @@ import axios from 'axios';
 
 
 const Main = () => {
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=ca13fd5cad80951f9021aee9c8656c29';
+    const [location, setLocation] = useState('');
+    const [data, setData] = useState({});
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ca13fd5cad80951f9021aee9c8656c29`;
 
-  return (
-    <div>
-      Main
-    </div>
-  )
+    
+
+    function sub(){
+        axios.get(url).then((response) => {
+            setData(response.data)
+            console.log(response.data)
+        })
+    }
+
+    return (
+      <div>
+          <input type="text" onChange={event => setLocation(event.target.value)} value={location}/>
+          <button onClick={sub}>submit</button>
+          {data.main? <h2>{data.main.temp}</h2> : null}
+      </div>
+    )
 }
 
 export default Main
